055d39aea7babd5392274874a85e8298954076f1
SaheelAtugade <sahilatugade212@gmail.com>
2025-07-02 23:35:54 +0530

    Update script.js removed to lowercase()

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function getWeather(cityName) {
         wind.textContent = `${data.wind.speed} Km/h`;
         input.value = "";
 
-        const condition = data.weather[0].main.toLowerCase();
+        const condition = data.weather[0].main;
         if (condition.includes("clouds")) {
             weatherImg.src = "./image/clouds.png";
         } else if (condition.includes("Rain")) {
